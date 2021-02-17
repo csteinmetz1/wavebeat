f0fec934c8a1c15780fc85e5225510b7d403ad92
csteinmetz1 <csteinmetz1@gmail.com>
2021-02-17 16:01:24 -0500

    fixing bug in GMSE loss

--- beat/loss.py
+++ beat/loss.py
@@ -16,6 +16,6 @@ class GlobalMSELoss(torch.nn.Module):
         target_no_beats = target[target == 0]
         input_no_beats = input[target == 0]
 
-        no_beat_loss = torch.nn.functional.mse_loss(input_beats, target_beats)
+        no_beat_loss = torch.nn.functional.mse_loss(target_no_beats, input_no_beats)
 
         return no_beat_loss + beat_loss, beat_loss, no_beat_loss
