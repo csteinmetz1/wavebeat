a0d4657739a1d99e85a3b330494f5ab1041b3507
csteinmetz1 <csteinmetz1@gmail.com>
2021-02-19 10:56:41 -0500

    adding additional peak picking options

--- beat/peak.py
+++ beat/peak.py
@@ -1,7 +1,15 @@
 import numpy as np
 import scipy.signal
 
-def find_beats(t, p, smoothing=15, threshold=0.5, distance=None, sample_rate=44100, beat_type="beat"):
+def find_beats(t, p, 
+                smoothing=127, 
+                threshold=0.5, 
+                distance=None, 
+                sample_rate=44100, 
+                beat_type="beat",
+                filter_type="cheby",
+                peak_type="simple"):
+    # 15, 2
 
     # t is ground truth beats
     # p is predicted beats 
@@ -9,23 +17,41 @@ def find_beats(t, p, smoothing=15, threshold=0.5, distance=None, sample_rate=441
     # 1 - beat
 
     N = p.shape[-1]
-    p = scipy.signal.savgol_filter(p, smoothing, 2)    # apply smoothing with savgol filter
-    p /= np.max(p)                                     # normalize the smoothed signal between 0.0 and 1.0
 
-    # by default, we assume that the min distance between beats is fs/4
-    # this allows for at max, 4 BPS, which corresponds to 240 BPM 
-    # for downbeats, we assume max of 1 downBPS
-    if beat_type == "beat":
-        if distance is None:
-            distance = sample_rate / 4
-    elif beat_type == "downbeat":
-        if distance is None:
-            distance = sample_rate
-    else:
-        raise RuntimeError(f"Invalid beat_type: `{beat_type}`.")
+    if filter_type == "savgol":
+        # apply smoothing with savgol filter
+        p = scipy.signal.savgol_filter(p, smoothing, 2)   
+    elif filter_type == "cheby":
+        sos = scipy.signal.cheby1(10,
+                                  1, 
+                                  100, 
+                                  btype='lowpass', 
+                                  fs=sample_rate,
+                                  output='sos')
+        p = scipy.signal.sosfilt(sos, p)
 
-    # perform peak picking given the supplied parameters
-    est_beats, heights = scipy.signal.find_peaks(p, height=threshold, distance=distance)
+    # normalize the smoothed signal between 0.0 and 1.0
+    p /= np.max(p)                                     
+
+    if peak_type == "simple":
+        # by default, we assume that the min distance between beats is fs/4
+        # this allows for at max, 4 BPS, which corresponds to 240 BPM 
+        # for downbeats, we assume max of 1 downBPS
+        if beat_type == "beat":
+            if distance is None:
+                distance = sample_rate / 4
+        elif beat_type == "downbeat":
+            if distance is None:
+                distance = sample_rate / 2
+        else:
+            raise RuntimeError(f"Invalid beat_type: `{beat_type}`.")
+
+        # apply simple peak picking
+        est_beats, heights = scipy.signal.find_peaks(p, height=threshold, distance=distance)
+
+    elif peak_type == "cwt":
+        # use wavelets
+        est_beats = scipy.signal.find_peaks_cwt(p, np.arange(1,50))
 
     # compute the locations of ground truth beats
     ref_beats = np.squeeze(np.argwhere(t==1).astype('float32'))
