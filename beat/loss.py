3740a08863facaaa67bf7b18090a2d4ce68bc5f2
csteinmetz1 <csteinmetz1@gmail.com>
2021-02-19 10:57:04 -0500

    adding a global BCE loss

--- beat/loss.py
+++ beat/loss.py
@@ -20,14 +20,44 @@ class GlobalMSELoss(torch.nn.Module):
 
         return no_beat_loss + beat_loss, beat_loss, no_beat_loss
 
-class FMeasureLoss(torch.nn.Module):
+class GlobalBCELoss(torch.nn.Module):
     def __init__(self):
-        super(FMeasureLoss, self).__init__()
+        super(GlobalBCELoss, self).__init__()
 
     def forward(self, input, target):
         
-        # beat errors
-        target_beats = target[target == 1]
-        input_beats = input[target == 1]
+        # split out the channels
+        beat_act_target = target[:,0,:]
+        downbeat_act_target = target[:,1,:]
 
-        precision = torch.nn.functional.mse_loss(input_beats, target_beats)
+        beat_act_input = input[:,0,:]
+        downbeat_act_input = input[:,1,:]
+
+        # beat errors
+        target_beats = beat_act_target[beat_act_target == 1]
+        input_beats =  beat_act_input[beat_act_target == 1]
+
+        beat_loss = torch.nn.functional.binary_cross_entropy(input_beats, target_beats)
+
+        # no beat errors
+        target_no_beats = beat_act_target[beat_act_target == 0]
+        input_no_beats = beat_act_input[beat_act_target == 0]
+
+        no_beat_loss = torch.nn.functional.binary_cross_entropy(input_no_beats, target_no_beats)
+
+        # downbeat errors
+        target_downbeats = downbeat_act_target[downbeat_act_target == 1]
+        input_downbeats = downbeat_act_input[downbeat_act_target == 1]
+
+        downbeat_loss = torch.nn.functional.binary_cross_entropy(input_downbeats, target_downbeats)
+
+        # no downbeat errors
+        target_no_downbeats = downbeat_act_target[downbeat_act_target == 0]
+        input_no_downbeats = downbeat_act_input[downbeat_act_target == 0]
+
+        no_downbeat_loss = torch.nn.functional.binary_cross_entropy(input_no_downbeats, target_no_downbeats)
+
+        # sum up losses
+        total_loss = beat_loss + no_beat_loss + downbeat_loss + no_downbeat_loss
+
+        return total_loss, beat_loss, no_beat_loss
